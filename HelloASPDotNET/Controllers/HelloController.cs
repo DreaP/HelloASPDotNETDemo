3055db7eddb4c2e8277e78470a7b23606ee66fa6
Pruitt <apruitt89@outlook.com>
2021-01-20 10:41:28 -0600

    Added Get and Route in controller

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        //Get :/<controller>/
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+
+        //Get Hello/Welcome
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app," + name + "!</h1>", "text/html");
+        }
+    }
+}
