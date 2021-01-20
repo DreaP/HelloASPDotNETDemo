942bb09bbac4dbaedda7876c4a25183498b4430d
Pruitt <apruitt89@outlook.com>
2021-01-20 11:43:50 -0600

    Added new attributes and form

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -13,16 +13,23 @@ namespace HelloASPDotNET.Controllers
         [Route("/helloworld/")]
         public IActionResult Index()
         {
-            string html = "<h1>Hello World!</h1>";
+
+            string html = "<form method='post' action='/helloworld/'>" + 
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' /> " +
+                "</form>";
+
             return Content(html, "text/html");
         }
 
         //Get Hello/Welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")]
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+        [HttpPost]
+        [Route("/helloworld/")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app," + name + "!</h1>", "text/html");
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
         }
     }
 }
